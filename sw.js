bc7f4ba80e8ecdd97bd83261d80f5ced9dbdde8e
963hal <150633086+963hal@users.noreply.github.com>
2026-09-10 23:08:30 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+/* UNCOVER P2 試作SW: index.htmlは常に最新を確認、画像はキャッシュ優先 */
+const CACHE = 'uncover-p2-v2';
+const ASSETS = ['./index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  const fresh = url.pathname.endsWith('index.html') || url.pathname.endsWith('/') || url.pathname.endsWith('.webmanifest');
+  if (fresh) {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+  } else {
+    e.respondWith(caches.match(e.request).then(hit => hit || fetch(e.request)));
+  }
+});
